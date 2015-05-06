51b27f797a49ac71cd5124369aabd8e9bd935a28
Alex <alex.turantsev@gmail.com>
2015-05-07 00:37:53 +0300

    First commit.
    Made grid, items.

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Grid : MonoBehaviour {
+	public int xSize, ySize;
+	public float itemWidth = 1f;
+	private GameObject[] _items;
+	private GridItem[,] _gridItems;
+	private GridItem _currentlySelected;
+
+	public static List<GridItem> selectedItems = new List<GridItem>();
+	public static bool selectedMode = false;
+	public static int selectedId;
+
+	private void Start () {
+		GetItems ();
+		FillGrid ();
+		GridItem.OnMouseDownItemEventHandler += OnMouseDownItem;
+		GridItem.OnMouseOverItemEventHandler += OnMouseOverItem;
+	}
+	
+	private void FillGrid(){
+		_gridItems = new GridItem[xSize, ySize];
+		for (var x = 0; x < xSize; x++) {
+			for(var y = 0; y < ySize; y++){
+				_gridItems[x,y] = InstantieItem(x,y);
+			}
+		}
+	}
+
+	private void Update(){
+		if (Input.GetMouseButtonUp (0)) {
+			//OnMouseUp();
+		}
+	}
+	
+	public void DeselectAll(){
+		foreach (var g in _gridItems) {
+			g.SetInactive(false);
+		}
+		for (var i = 0; i < selectedItems.Count; i++) {
+			selectedItems[i].Selected(false);
+		}
+		selectedItems.Clear ();
+	}
+
+	private void OnMouseDownItem(GridItem item){
+		if (!selectedMode) {
+			foreach (var g in _gridItems) {
+				if (g.id != item.id)
+					g.SetInactive (true);
+			}
+			selectedMode = true;
+			selectedId = item.id;
+			return;
+		} else {
+			if(item.id != selectedId || selectedItems.Count < 4){
+				selectedMode = false;
+				DeselectAll();
+				return;
+			}
+			//if items id is right and line consist of 4 and more items, destroy them
+			selectedMode = false;
+			DeselectAll();
+
+			return;
+		}
+	}
+
+	private void OnMouseOverItem(GridItem item){
+		if (!selectedMode || item.inactive)
+			return;
+		var itemS = selectedItems.Last ();
+		if (itemS == item)
+			return;
+		if (GridItem.isNearestItems (item, itemS)) {
+			item.Selected(true);
+		}
+	}
+
+	private GridItem InstantieItem(int x, int y){
+		var item = _items [Random.Range (0, _items.Length)];
+		var newItem = Instantiate (item, new Vector3 (x * itemWidth, y), Quaternion.identity) as GameObject;
+		var gridItem = newItem.GetComponent<GridItem> ();
+		gridItem.OnItemPositionChanged (x, y);
+		return gridItem;
+	}
+
+	private void GetItems(){
+		_items = Resources.LoadAll<GameObject>("Prefabs");
+		for (var i = 0; i < _items.Length; i++) {
+			_items[i].GetComponent<GridItem>().id = i;
+		}
+	}
+}
---
+++ Assets/Scripts/GridItem.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridItem : MonoBehaviour {
+	/*public int x {
+		get;
+		private set;
+	}
+
+	public int y {
+		get;
+		private set;
+	}*/
+	public int x;
+	public int y;
+	public bool selected = false;
+	public bool inactive = false;
+
+	public int id;
+	public Rigidbody2D rigid;
+	public SpriteRenderer spriteRenderer;
+	public GameObject selectedSprite;
+
+	public delegate void OnMouseDownItem(GridItem item);
+	public static event OnMouseDownItem OnMouseDownItemEventHandler;
+
+	public delegate void OnMouseOverItem(GridItem item);
+	public static event OnMouseOverItem OnMouseOverItemEventHandler;
+
+	private void Start(){
+		rigid = gameObject.GetComponent<Rigidbody2D> ();
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
+		selectedSprite.SetActive (false);
+	}
+
+	public void OnItemPositionChanged(int newx, int newy){
+		x = newx;
+		y = newy;
+		gameObject.name = string.Format ("Item[{0}][{1}]", newx, newy);
+	}
+
+	private void OnMouseDown(){
+		Selected (true);
+		if (OnMouseDownItemEventHandler != null) {
+			OnMouseDownItemEventHandler(this);
+		}
+	}
+
+	private void OnMouseOver(){
+		if (OnMouseOverItemEventHandler != null)
+			OnMouseOverItemEventHandler (this);
+	}
+
+	public void SetInactive(bool inactive){
+		this.inactive = inactive;
+		var c = spriteRenderer.color;
+		var opacity = inactive ? 0.2f : 1f;
+		spriteRenderer.color = new Color (c.r, c.g, c.b, opacity);
+	}
+
+	public void Selected(bool selected){
+		selectedSprite.SetActive (selected);
+		selected = selected;
+		Grid.selectedItems.Add (this);
+	}
+
+	public static bool isNearestItems(GridItem a, GridItem b){
+		var max = Mathf.Max (Mathf.Abs (a.x - b.x), Mathf.Abs (a.y - b.y));
+		if (max == 1)
+			return true;
+		return false;
+
+	}
+}
